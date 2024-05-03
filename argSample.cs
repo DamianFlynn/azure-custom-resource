5f83ac87a21100c6d639391be47b6d280a834dd9
Damian Flynn <info@damianflynn.com>
2024-05-03 23:04:56 +0100

    fix: initial action test code

--- argSample.cs
+++ argSample.cs
@@ -15,10 +15,37 @@ namespace CustomProvider.Action
         }
 
         [Function("argSample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/Microsoft.CustomProviders/resourceproviders/{minirpname}/{action}")] HttpRequest req,
+        string subscriptionId,
+        string resourceGroupName,
+        string minirpname,
+        string action)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            _logger.LogInformation($"The Custom Provider Function 'HttpTrigger2' received a request '{req.Method}'.");
+            _logger.LogInformation($"The miniRpName was: '{minirpname}'");
+            _logger.LogInformation($"The action was: '{action}'");
+            _logger.LogInformation($"The HTTP Method was: '{req.Method}'");
+            
+
+            if (action == "ping")
+            {
+                if (req.Method != HttpMethod.Post.Method)
+                {
+                    return new StatusCodeResult(StatusCodes.Status405MethodNotAllowed);
+                }
+                else
+                {
+                    var host = req.Headers["Host"].FirstOrDefault() ?? "anonymous";
+                     _logger.LogInformation($"The host was: '{host}'");
+                    var content = $"{{ 'pingcontent' : {{ 'source' : '{host}' }} , 'message' : 'hello {host}'}}";
+                    return new OkObjectResult(content);
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+
         }
     }
 }
